16a276535fa7ce8ecc980d4b6e1a381d32571859
Mark Blanco <mark.p.blanco@gmail.com>
2018-06-19 10:16:55 -0400

    Add initial setup script for BLE W0 nodes.

---
+++ setup_W0.sh
@@ -0,0 +1,28 @@
+if [ $# -eq 1 ]
+then
+	if [ $1 -gt 9 ]
+	then
+		nodename="ble_mobility_$1"
+	else
+		nodename="ble_mobility_0$1"
+	fi
+	echo "Setting Pi W0 up with hostname $nodename"
+	hostname $nodename
+else
+	echo "No argument supplied!"
+	echo "Run as: `basename "$0"` <number of BLE sensing node>"
+	exit 1
+fi
+
+echo "Updating system with required packages."
+echo "This may take a while..."
+git pull
+sudo apt-get update
+sudo apt-get install -y vim tmux python-dev python-pip python-bluepy hcitool
+echo "Done installing packages!"
+
+# Todo: Add sensing script to /etc/rc.local so logging starts on startup
+# Todo: Add periodic upload to collection server in crontab or similar
+# Todo: Detect if node is outdoor node based on presence of ADC on SPI bus
+# Todo: Add power management (auto sleep, wakeup, shutdown) if node is outdoor node
+#		This may include frequency governor and increased polling period for BLE sensing.
