c3978189b8e2c6950168b431ce28e3f144d4e97b
Peng Chen <pengc1@andrew.cmu.edu>
2018-07-06 07:02:17 +0000

    Remove password of pi and adding comment

--- node-setup.sh
+++ node-setup.sh
@@ -22,14 +22,12 @@ echo "This may take a while..."
 
 # rsync daily data
 
-#ssh_cmd="ssh pi@ble-mobility-${number}.wv.cc.cmu.edu"
 rsync_cmd="rsync -rvz -Pav -e \"ssh -i /home/pi/.ssh/id_rsa\" --progress pi@ble-mobility-${number}.wv.cc.cmu.edu:/home/pi/ble_campus_mobility_sensing/passive_ble/data /home/pi/data_collection/${nodename}"
-#echo $ssh_cmd >> /home/pi/ble_campus_mobility_sensing/collect-daily.sh 
 echo $rsync_cmd >> /home/pi/ble_campus_mobility_sensing/collect-daily.sh
 
 # setup mail function for each deployed node
 sudo ./mail-setup.sh $1
 
-# run tmux
+# Setup running tmux for every deployed node after reboot
 sudo ./run-tmux.sh $1
 
--- run-tmux.sh
+++ run-tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# run the code in deployed node automatically
+# run the code in deployed node automatically in tmux after reboot, so even if the node is plugged off accidentally, after plugging it on the ble code will run again.
+
 run_tmux () {
 	touch tmux_start.sh
 	header_cmd="#!/bin/bash"
@@ -8,18 +9,9 @@ run_tmux () {
 	echo $header_cmd >> /home/pi/tmux_start.sh
 	echo $tmux_cmd >> /home/pi/tmux_start.sh
 	chmod +x tmux_start.sh
+	# add command to /etc/rc.local
 	sudo sed -i -e '$i \sudo bash /home/pi/tmux_start.sh &\n' /etc/rc.local
-	# start_cmd="sudo -u pi bash /home/pi/tmux_start.sh &"
-	# echo $start_cmd >> /etc/rc.local
 	sudo reboot
-	# tmux 
-	# cd ble_campus_mbility_sensing/passive_ble/sensing_src/
-	# sudo ./ble_passive_sniffer.py 
-	# nohup sudo ./ble_passive_sniffer.py &
-    	# nohup sudo ./ble_passive_sniffer.py > /dev/null 2>&1 &
-	# tmux detach
-	# builtin exit
-
 }
 
 if [ $# -eq 1 ]
@@ -36,6 +28,6 @@ else
 fi
 
 
-# run code in tmux
+# ssh to each deployed node
 ssh -i /home/pi/.ssh/id_rsa pi@ble-mobility-${number}.wv.cc.cmu.edu "$(typeset -f run_tmux); run_tmux"
 
